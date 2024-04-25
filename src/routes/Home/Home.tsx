8eb88e60446cffc37fad7d52f675217dfeaa0430
HoudAlami <houd.alami-marrouni@oclock.school>
2024-04-26 00:17:32 +0200

    fetch de la data, next step creer un pokemon aleatoire au chargement de la page

--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -1,8 +1,27 @@
-import React from 'react';
+/* eslint-disable no-console */
+import React, { useEffect, useState } from 'react';
 import './Home.scss';
+import axios from 'axios';
 import PokemonRandom from '../../components/PokemonRandom/PokemonRandom';
 
 function Home() {
+  const [data, setData] = useState([]);
+  // const [randomItem, setRandomItem] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          'https://pokeapi.co/api/v2/pokemon?limit=151&offset=0'
+        );
+        setData(response.data);
+        console.log(response);
+      } catch (error) {
+        console.error('erreur du fetch', error);
+      }
+    };
+    fetchData();
+  }, []);
   return (
     <div className="home">
       <h1 className="home__title">Pokedex</h1>
