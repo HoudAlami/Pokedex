1474b7fdd8e4f23c5ebcb1260b3ef5b15f3daf55
HoudAlami <houd.alami-marrouni@oclock.school>
2024-05-12 22:40:05 +0200

    random character ok, style a faire

--- src/components/PokemonRandom/PokemonRandom.tsx
+++ src/components/PokemonRandom/PokemonRandom.tsx
@@ -1,30 +1,44 @@
 import React from 'react';
 import './PokemonRandom.scss';
 
-function PokemonRandom() {
+type Character = {
+  id: number;
+  name: string;
+  race: string;
+  description: string;
+  image: string;
+};
+
+type PokemonRandomProps = {
+  characters: Character[];
+};
+
+function PokemonRandom({ characters }: PokemonRandomProps) {
+  const randomIndex = Math.floor(Math.random() * characters.length);
+  const randomCharacter = characters[randomIndex];
+  // console.log(randomCharacter);
+
   return (
     <div className="pokemonRandom">
-      <h3 className="pokemonRandom__title">PokemonRandom</h3>
-      <div className="pokemonRandom__info">
-        <h3 className="pokemonRandom__name">Kinouss #148</h3>
-        <p className="pokemonRandom__type">Type hnini</p>
-      </div>
-      <div className="pokemonRandom__description">
-        <p className="pokemonRandom__description-text">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi libero
-          excepturi ad incidunt soluta doloremque accusantium harum expedita ab
-          vero. Nobis magni ratione cum, architecto laudantium dolore quos
-          libero qui? Nisi dolores aspernatur facilis maiores illum consequatur
-          cupiditate fugit ipsum, aliquid quidem recusandae reprehenderit esse
-          similique porro reiciendis quam! Ut animi facere dignissimos quos
-          excepturi accusamus facilis explicabo eligendi nostrum.
-        </p>
-        <img
-          src="#"
-          alt="pokemon"
-          className="pokemonRandom__description-image"
-        />
-      </div>
+      <h3 className="pokemonRandom__title">Personnage Aléatoire</h3>
+      {randomCharacter && ( // Vérification si un personnage est disponible
+        <div>
+          <div className="pokemonRandom__info">
+            <h3 className="pokemonRandom__name">{randomCharacter.name}</h3>
+            <p className="pokemonRandom__type">{randomCharacter.race}</p>
+          </div>
+          <div className="pokemonRandom__description">
+            <p className="pokemonRandom__description-text">
+              {randomCharacter.description}
+            </p>
+            <img
+              src={randomCharacter.image} // Utilisation de l'image du personnage
+              alt={randomCharacter.name}
+              className="pokemonRandom__description-image"
+            />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -1,34 +1,51 @@
-/* eslint-disable no-console */
 import React, { useEffect, useState } from 'react';
 import './Home.scss';
 import axios from 'axios';
 import PokemonRandom from '../../components/PokemonRandom/PokemonRandom';
 
+type Character = {
+  id: number;
+  name: string;
+  race: string;
+  description: string;
+  image: string;
+};
+
 function Home() {
-  const [data, setData] = useState([]);
-  // const [randomItem, setRandomItem] = useState(null);
+  const [characters, setCharacters] = useState<Character[]>([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get(
-          'https://pokeapi.co/api/v2/pokemon?limit=151&offset=0'
+          'https://dragonball-api.com/api/characters?limit=58'
         );
-        setData(response.data);
-        console.log(response);
+        console.log(response.data);
+
+        // Vérifier si la réponse contient bien un objet avec une propriété 'items'
+        if (response.data && response.data.items) {
+          // Extraire le tableau de personnages de la propriété 'items'
+          const charactersData = response.data.items;
+          // Mettre à jour l'état avec les personnages
+          setCharacters(charactersData);
+        } else {
+          console.error("La réponse de l'API est invalide :", response.data);
+        }
       } catch (error) {
-        console.error('erreur du fetch', error);
+        console.error('Erreur lors du fetch :', error);
       }
     };
     fetchData();
   }, []);
+
   return (
     <div className="home">
       <h1 className="home__title">Pokedex</h1>
       <div className="home__item">
         <h2 className="home__item-title">Liste des Pokemons</h2>
       </div>
-      <PokemonRandom />
+      {/* Passer le tableau characters au composant PokemonRandom */}
+      <PokemonRandom characters={characters} />
     </div>
   );
 }
